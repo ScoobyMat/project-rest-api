355b7585b5ea2c57db48c5da0e5bf4d43d691f7c
Scooby <mateusz.skubiszak1@wp.pl>
2024-04-02 16:05:07 +0200

    Adds config

--- project-rest-api/src/main/java/com/project/rest/config/SecurityConfig.java
+++ project-rest-api/src/main/java/com/project/rest/config/SecurityConfig.java
@@ -1,4 +1,20 @@
 package com.project.rest.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
 }
